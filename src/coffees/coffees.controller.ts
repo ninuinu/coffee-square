63c463af64d032e619580ce479bbf33b342a06c4
Helena Rosenzweig <helena.rosenzw@gmail.com>
2022-10-20 23:15:58 +0200

    remove console logs

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -32,8 +32,6 @@ export class CoffeesController {
   @Post()
   @HttpCode(HttpStatus.GONE)
   create(@Body() createCoffeeDto: CreateCoffeeDto) {
-    console.log("hello!");
-    console.log(createCoffeeDto instanceof CreateCoffeeDto);
     return this.coffeesService.create(createCoffeeDto);
   }
 
