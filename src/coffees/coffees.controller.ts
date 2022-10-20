189f792e1b842a046be4cb655cc5847916e427d5
Helena Rosenzweig <helena.rosenzw@gmail.com>
2022-10-20 23:25:18 +0200

    remove console logs

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -25,8 +25,9 @@ export class CoffeesController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.coffeesService.findOne(id);
+  findOne(@Param('id') id: number) {
+    console.log(typeof id);
+    return this.coffeesService.findOne('' + id);
   }
 
   @Post()
