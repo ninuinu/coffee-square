29bedc27e64f71399b335a11d833358954a8ee2b
Helena Rosenzweig <helena.rosenzw@gmail.com>
2022-10-23 23:11:57 +0200

    refactor coffees service so that relations are included at retrieval

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -14,6 +14,6 @@ export class Coffee {
   brand: string;
 
   @JoinTable()
-  @ManyToMany((type) => Flavor, (flavor) => flavor.coffees)
-  flavors: string[];
+  @ManyToMany((type) => Flavor, (flavor) => flavor.coffees, { cascade: true })
+  flavors: Flavor[];
 }
